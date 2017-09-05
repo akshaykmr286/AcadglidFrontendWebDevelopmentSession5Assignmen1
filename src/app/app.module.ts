3994def4de9e2b2527ed773e260ad07cd17cc463
Akshay Kumar Nayak <akshaykmr286@gmail.com>
2017-09-05 17:04:38 +0530

    my third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RootComponentComponent } from './root-component/root-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponentComponent
   ],
   imports: [
     BrowserModule
